12dad82f40698b141c41f757825fa5be63cfe6a4
Aden Durney <adendurney@gmail.com>
2021-04-29 21:49:35 +1200

    attempt at building envelope

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as plt
 MAX_HARMONIC = 91
 
 
-TIME_RISE = 100e-9 # seconds
-TIME_FALL = 100e-12 #seconds
+TIME_RISE = 100 # seconds
+TIME_FALL = 100e-9 #seconds
+TIME_MIN = min(TIME_RISE,TIME_FALL)
 CARRIER_FREQ = 1e6 #hz
-AMPLITUDE = 1e-3#amps
+PERIOD = 1/CARRIER_FREQ
+AMPLITUDE = 1#amps
 DUTY_CYCLE = 0.5
 LOOP_AREA = 10e-3 * 10e-3 #m^2
+
 
 
 def harmonic_current(amplitude,t_min,duty,carrier_freq,harmonic):
@@ -24,6 +27,7 @@ def harmonic_current(amplitude,t_min,duty,carrier_freq,harmonic):
     sine2 = (sin(term2) / term2)
 
     harmonic_current = 2 * amplitude * duty * sine1 * sine2
+    #return sin(term1)
     return harmonic_current
 
 def harmonic_field_strength(freq,loop_area, current):
@@ -31,24 +35,36 @@ def harmonic_field_strength(freq,loop_area, current):
     return field_strength
 
 
+def harmonic_current_envelope(first_harmonic_current, t_min):
+    pass
+
 
 harmonics = range(1,MAX_HARMONIC,2)
 harmonics_current = []
+
 harmonics_str = []
 
 
 fig,ax = plt.subplots()
 
+#TODO hardcoded for now
+envelope_x = [1,1/(pi*TIME_MIN)]
+
+envelope_y = [0.64,]
+
 for harm in harmonics:
-    current = harmonic_current(AMPLITUDE,TIME_RISE,DUTY_CYCLE,CARRIER_FREQ,harm)
-    harmonics_current.append(current*1e3)
+    current = harmonic_current(AMPLITUDE,TIME_MIN,DUTY_CYCLE,CARRIER_FREQ,harm)
+    harmonics_current.append(abs(current))
     field_str = harmonic_field_strength(CARRIER_FREQ * harm,LOOP_AREA,current)
     harmonics_str.append(field_str)
 
 
+
+
 ax.stem(harmonics,harmonics_current)
-ax.grid(True)
 ax.plot()
+
+ax.grid(True)
 
 
 
@@ -56,7 +72,7 @@ ax.plot()
 plt.xlabel("nth harmonic")
 plt.ylabel("Current (mA)")
 plt.title("Harmonic current of a pure square wave")
-#plt.ylim(top=abs(harmonics_current[4])*1.1)
+plt.xscale("log")
 
 
 plt.show()
